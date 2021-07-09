b8bb2815ae64a3bb8e1dc089b466bedc3f16f0df
wickslayer <80254133+wickslayer@users.noreply.github.com>
2021-07-09 17:01:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+
+var track,carImg1,carImg2,carImg3,carImg4,ground;
+
+function preload(){
+  
+track=loadImage("images/track.jpg");
+carImg1=loadImage("images/car1.png");
+carImg2=loadImage("images/car2.png");
+carImg3=loadImage("images/car3.png");
+carImg4=loadImage("images/car4.png");
+ground=loadImage("images/ground.png");
+  
+
+
+
+}
+
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState ===2){
+    game.end();
+  }
+}
